b03fcc3aa107981bd3691dfb9e96a7a503c9359d
delgado <alex.o.delgado@gmail.com>
2024-02-04 12:05:43 -0500

    Run one test at a time to limit pressure on memory

--- polars-rust/run_rust.sh
+++ polars-rust/run_rust.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Build and run rust code to generate unbalances and completness test
 cargo run --release -- \
-  --gl=../gitignore/data/gl.parquet/gl_*.parquet \
-  --tb=../gitignore/data/tb.parquet/tb_*.parquet \
+  --gl=../gitignore/data/gl.parquet/gl_00*.parquet \
+  --tb=../gitignore/data/tb.parquet/tb_00*.parquet \
   --output=results
+# Dont run wide open our will run out of RAM
--- polars-rust/src/main.rs
+++ polars-rust/src/main.rs
@@ -9,7 +9,7 @@ use crate::{
 use clap::Parser;
 use color_eyre::eyre;
 use packs::{completeness_test, unbalanced_journal_entries_test};
-use polars::prelude::{collect_all, DataFrame, LazyFrame, PolarsError};
+use polars::prelude::{DataFrame, LazyFrame};
 use std::path::PathBuf;
 
 #[derive(Debug, Parser)]
@@ -41,29 +41,34 @@ fn main() -> Result<(), eyre::Error> {
     let unbalanced: LazyFrame = unbalanced_journal_entries_test(&gl);
     let completeness: LazyFrame = completeness_test(&gl, &tb);
 
-    // Run unbalanced and completeness test
-    let results: Vec<DataFrame> = collect_all([unbalanced, completeness])?;
-    let mut results_iter = results.into_iter();
-
-    let mut unbalanced: DataFrame = results_iter.next().ok_or(PolarsError::NoData(
-        "Unbalanced results are missing.".into(),
-    ))?;
-    let mut completeness: DataFrame = results_iter.next().ok_or(PolarsError::NoData(
-        "Completeness results are missing.".into(),
-    ))?;
-
     // Reset the output folder location
     reset_output(&args.output_path)?;
 
-    // Write the results to Parquet
-    write_parquet(
-        &mut unbalanced,
-        &args.output_path.join("unbalanced.parquet"),
-    )?;
-    write_parquet(
-        &mut completeness,
-        &args.output_path.join("completeness.parquet"),
-    )?;
+    // Run Unbalanced test and write the results to Parquet. Also make sure to remove Dataframe from
+    // memory before next test runs.
+    {
+        println!("Running Unbalanced test");
+        let mut unbalanced: DataFrame = unbalanced.collect()?;
+        println!("Writing Unbalanced results");
+        write_parquet(
+            &mut unbalanced,
+            &args.output_path.join("unbalanced.parquet"),
+        )?;
+        println!("Unbalanced results writen");
+    }
+
+    // Run Completeness test and write the results to Parquet. Also make sure to remove Dataframe from
+    // memory before next test runs.
+    {
+        println!("Running Completeness test");
+        let mut completeness: DataFrame = completeness.collect()?;
+        println!("Writing Completeness results");
+        write_parquet(
+            &mut completeness,
+            &args.output_path.join("completeness.parquet"),
+        )?;
+        println!("Completeness results writen");
+    }
 
     Ok(())
 }
