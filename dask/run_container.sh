6b51eb2bff85cc9825f70bf553f64da8a07a0919
delgado <alex.o.delgado@gmail.com>
2024-02-18 04:24:57 -0500

    Containerize DASK test

--- dask/run_container.sh
+++ dask/run_container.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 # Build image to generate a test data
-echo "Building PySpark" &&
+echo "Building DASK" &&
 podman build \
-  --tag pyspark:latest \
+  --tag dask:latest \
   . &&
 echo &&
 
 # Run Data-Generator to stack seed GL / TB data to 1 billion rows
-echo "Running PySpark" &&
+echo "Running DASK" &&
 podman run \
   --rm \
   --cpus=4 \
   --memory=32G \
-  --volume ../gitignore/data:/opt/pyspark/data:z \
-  --volume ./gitignore:/opt/pyspark/gitignore:z \
-  pyspark:latest \
+  --volume ../gitignore/data:/opt/dask/data:z \
+  --volume ./gitignore:/opt/dask/gitignore:z \
+  --publish 8787:8787 \
+  dask:latest \
   --gl=./data/gl.parquet/ \
   --tb=./data/tb.parquet/ \
   --output=./gitignore/results/ &&
